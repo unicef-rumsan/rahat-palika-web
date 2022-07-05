6b7ec635cc067b406cbae74dfd5e648c80abccaa
Raktim Shrestha <raktim@rumsan.com>
2022-07-06 00:52:11 +0545

    adding proper case utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,6 @@
 import { ROLES } from '../constants';
 import EthCrypto from 'eth-crypto';
 
-
 const DEF_SHOW_CHARS = 20;
 
 export const renderSingleRole = roles => {
@@ -62,23 +61,27 @@ export const formatErrorMsg = err => {
 	return errMessage;
 };
 
-
-export const getRandomString= (length) => {
-		let randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-		let result = '';
-		for (let i = 0; i < length; i++) {
-			result += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
-		}
-		return result;
+export const getRandomString = length => {
+	let randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	let result = '';
+	for (let i = 0; i < length; i++) {
+		result += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
 	}
+	return result;
+};
 
 export const getRandomEntropy = () => {
-	const randomChars = getRandomString(128)
-		return Buffer.from(randomChars, 'utf-8');
-	}
+	const randomChars = getRandomString(128);
+	return Buffer.from(randomChars, 'utf-8');
+};
 
 export const createRandomIdentity = () => {
 	const entropy = getRandomEntropy();
 	return EthCrypto.createIdentity(entropy);
+};
 
-}
+export const getTitleCase = str => {
+	return str.replace(/\w\S*/g, function (txt) {
+		return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+	});
+};
